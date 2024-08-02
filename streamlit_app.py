ccb3522e7f0f44698581465780c60bea3bf822f5
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-08-02 15:34:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,18 +8,18 @@ st.write(
 
 # Khởi tạo hoặc lấy lại data từ session state
 if 'data' not in st.session_state:
-    st.session_state.data = pd.DataFrame(columns=['length', 'deepth', 'sex'])
+    st.session_state.data = pd.DataFrame(columns=['height', 'weight', 'sex'])
 
 # Tạo các công cụ nhập liệu
 st.title("Data Entry Form")
 
-length = st.slider("Select length", min_value=1, max_value=100, value=1)
-deepth = st.slider("Select deepth", min_value=1, max_value=50, value=1)
+height = st.slider("Select height", min_value=10, max_value=120, value=1)
+weight = st.slider("Select weight", min_value=110, max_value=200, value=1)
 sex = st.selectbox("Select sex", ["male", "female"])
 
 # Nút để thêm dữ liệu vào dataframe
 if st.button("Add Data"):
-    new_row = pd.DataFrame({'length': [length], 'deepth': [deepth], 'sex': [sex]})
+    new_row = pd.DataFrame({'height': [height], 'weight': [weight], 'sex': [sex]})
     st.session_state.data = pd.concat([st.session_state.data, new_row], ignore_index=True)
     st.success("Data added successfully!")
 
